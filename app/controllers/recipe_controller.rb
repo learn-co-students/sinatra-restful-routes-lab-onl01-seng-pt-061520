b42996b6489a8481a849bdf1b2bedfe8bd92f5a7
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-10 14:12:24 -0400

    Done.

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,7 +1,26 @@
 class RecipeController < ApplicationController
-
   get '/recipes' do
-    erb :'/recipes/index'
+    erb :'recipes/index'
   end
 
+  get '/recipes/new' do
+    erb :'recipes/new'
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.new
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    if @recipe.save
+      redirect "/recipes/#{@recipe.id}"
+    else
+      erb :'recipes/new'
+    end
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :'recipes/show' if @recipe
+  end
 end
